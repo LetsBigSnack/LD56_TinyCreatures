411b5f88c08813a4b20b7eab18a0058fd246c756
Jhyke <igorduifhuizen@gmail.com>
2024-10-06 13:56:45 +0200

    Popup settings main scene

---
+++ Assets/PopupHelper.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupHelper : MonoBehaviour
+{
+    private PopupManager popupManager;
+
+    private void Awake()
+    {
+        popupManager = FindObjectOfType<PopupManager>();
+    }
+
+    public void SettingsBtnPressed()
+    {
+        popupManager.ViewPopup(0);
+    }
+}
