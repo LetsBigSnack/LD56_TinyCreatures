9d8f767a5254dd250b57f54ce5412fab1d844ee2
LetsBigSnack <83255386+LetsBigSnack@users.noreply.github.com>
2024-10-07 18:59:46 +0200

    Color changing

--- Assets/UI_CreatureDetailsText.cs
+++ Assets/UI_CreatureDetailsText.cs
@@ -19,10 +19,15 @@ public class UI_CreatureDetailsText : MonoBehaviour
         creatureNameText.text = null;
         powerLevelText.text = null;
         hpText.text = null;
+        hpText.color = new Color(6/255f,40/255f,41/255f,255);
         atkText.text = null;
+        atkText.color = new Color(6/255f,40/255f,41/255f,255);
         spdText.text = null;
+        spdText.color = new Color(6/255f,40/255f,41/255f,255);
         dexText.text = null;
+        dexText.color = new Color(6/255f,40/255f,41/255f,255);
         defText.text = null;
+        defText.color = new Color(6/255f,40/255f,41/255f,255);
     }
 
     public void SetupRepresentation(Creature creature)
@@ -34,11 +39,90 @@ public class UI_CreatureDetailsText : MonoBehaviour
         
         creatureNameText.text = creature.CreatureName;
         powerLevelText.text = creature.PowerLevel.ToString();
-        hpText.text = creature.MaxHealth.ToString("n2"); 
-        atkText.text = creature.Attack.ToString("n2"); 
-        spdText.text = creature.Speed.ToString("n2"); 
-        dexText.text = creature.Dexterity.ToString("n2"); 
-        defText.text = creature.Defense.ToString("n2"); 
+        hpText.text = creature.MaxHealth.ToString("n2");
+        hpText.color = new Color(6/255f,40/255f,41/255f,255);
+        atkText.text = creature.Attack.ToString("n2");
+        atkText.color = new Color(6/255f,40/255f,41/255f,255);
+        spdText.text = creature.Speed.ToString("n2");
+        spdText.color = new Color(6/255f,40/255f,41/255f,255);
+        dexText.text = creature.Dexterity.ToString("n2");
+        dexText.color = new Color(6/255f,40/255f,41/255f,255);
+        defText.text = creature.Defense.ToString("n2");
+        defText.color = new Color(6/255f,40/255f,41/255f,255);
+    }
+
+    public void CompareColor(Creature creature1, Creature creature2)
+    {
+
+        Color pos = new Color(10/255f,27/255f,1/255f);
+        Color neg = new Color(90/10f,13/255f,6/255f);
+        
+        if (creature1 == null || creature2 == null)
+        {
+            return;
+        }
+
+
+        if (creature1.MaxHealth != creature2.MaxHealth)
+        {
+            if (creature1.MaxHealth < creature2.MaxHealth)
+            {
+                hpText.color = neg;
+            }
+            else
+            {
+                hpText.color = pos;
+            }
+        }
+        
+        if (creature1.Attack != creature2.Attack)
+        {
+            if (creature1.Attack < creature2.Attack)
+            {
+                atkText.color = neg;
+            }
+            else
+            {
+                atkText.color = pos;
+            }
+        }
+        
+        if (creature1.Speed != creature2.Speed)
+        {
+            if (creature1.Speed < creature2.Speed)
+            {
+                spdText.color = neg;
+            }
+            else
+            {
+                spdText.color = pos;
+            }
+        }
+        
+        if (creature1.Dexterity != creature2.Dexterity)
+        {
+            if (creature1.Dexterity < creature2.Dexterity)
+            {
+                dexText.color = neg;
+            }
+            else
+            {
+                dexText.color = pos;
+            }
+        }
+        
+        if (creature1.Defense != creature2.Defense)
+        {
+            if (creature1.Defense < creature2.Defense)
+            {
+                defText.color = neg;
+            }
+            else
+            {
+                defText.color = pos;
+            }
+        }
+        
     }
 
 }
